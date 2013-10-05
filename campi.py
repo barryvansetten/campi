79d81ab933c95bee061186815bbe08d30f694afc
barryvansetten <barry.vansettten@gmail.com>
2013-10-05 16:24:41 +0200

    changed to code to better match python styleguide

--- campi.py
+++ campi.py
@@ -1,39 +1,48 @@
-import os, datetime
+import os
+import datetime
 
-class campi:
+
+class Campi:
     """Class for controlling the Pi Camera"""
+
     def __init__(self):
         #the directory where the pictures are stored
-        self.targetDirectory = os.getenv("HOME") + "/Pictures/campi/" + datetime.datetime.now().strftime("%Y%m%dT%H%M%S")
+        self.__target_directory = (os.getenv("HOME") + "/Pictures/campi/"
+        + datetime.datetime.now().strftime("%Y%m%dT%H%M%S"))
 
         self.width = 1024
         self.height = 768
-        self.prefix = "campi-"
+
+        #the prefix of the file name
+        self.__prefix = "campi-"
         
         #the number of pictures that were made
-        self.pictureCounter = 0
+        self.__picture_counter = 0
 
-    def filename(self):
-        self.pictureCounter+=1
-        return self.prefix + str(self.pictureCounter) + ".jpg"
+    def __generate_filename(self):
+        self.__picture_counter+=1
+        return self.__prefix + str(self.__picture_counter) + ".jpg"
         
     def shoot(self):
         """ Take picture"""
-
         #if no pictures have been made yet then it is first checked of the target folder exists
-        if self.pictureCounter == 0:
-            if not os.path.exists(self.targetDirectory):
-                os.makedirs(self.targetDirectory)
+        if self.__picture_counter == 0:
+            if not os.path.exists(self.__target_directory):
+                os.makedirs(self.__target_directory)
 
+        #generate the raspistill command for taking the pictures
+        shootCommand = ("raspistill -w " + str(self.width) + " -h " +
+        str(self.height) + " -o " + self.__target_directory +
+        "/" + self.__generate_filename())
+
+        #take the pictures
+        print(shootCommand)       
+        #os.system(shootCommand)
         
 
-        shootCommand = "raspistill -w " + str(self.width) + " -h " + str(self.height) + " -o " + self.targetDirectory + "/" + self.filename()
-        #print(shootCommand)       
-        os.system(shootCommand)
-        
 
 
-
-cam = campi()
+cam = Campi()
 cam.shoot()
+
 
